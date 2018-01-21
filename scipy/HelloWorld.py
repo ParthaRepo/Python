76fd3870f7326ec52c8b941aff5abdf21fbe7395
pxl8108 <parthasarathi_logakannan@homedepot.com>
2018-01-20 20:30:17 -0500

    statements, conditions, loops

--- scipy/HelloWorld.py
+++ scipy/HelloWorld.py
@@ -1,7 +1,26 @@
-#This program will ask my name and say hello in response
-print("Please endter your name : ")
+#This is the sample program for how statement, loops and conditions works in python.
+print("Please enter your name : ")
 name = input()
 print("Hello ", name)
-print("How are you?")
-response = input()
-print("Nice to hear back from you. Thank you.")
+#This block of code will execute , if the user is partha
+if name == 'partha':
+    print("Nice to hear back from you. partha!!!")
+elif name == 'Partha':
+    print("Nice to hear back from you. Partha!!!")
+else:
+    print("How are you?")
+# for loop
+print("Print name 5 time")
+for i in range(5):
+    print(name)
+#While loop
+print("Enter the number to test While: ")
+number = int(input())
+while number<5:
+    number = number+1
+    print("increment number by one:" ,number)
+#Break if name is partha
+    if name == 'partha':
+        break
+
+
