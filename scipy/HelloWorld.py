6833b1cfd35c6fa3ab12e346d2a5f6516bf2b922
pxl8108 <parthasarathi_logakannan@homedepot.com>
2018-01-20 19:56:10 -0500

    Hello world initial commit

---
+++ scipy/HelloWorld.py
@@ -0,0 +1,7 @@
+#This program will ask my name and say hello in response
+print("Please endter your name : ")
+name = input()
+print("Hello ", name)
+print("How are you?")
+response = input()
+print("Nice to hear back from you. Thank you.")
